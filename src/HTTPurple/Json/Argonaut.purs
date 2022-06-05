36b696adcf9bfb035fc63ed57496d6daf3b7d4e2
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-06-05 20:44:16 +0100

    Add encoder

--- src/HTTPurple/Json/Argonaut.purs
+++ src/HTTPurple/Json/Argonaut.purs
@@ -2,13 +2,18 @@ module HTTPurple.Json.Argonaut where
 
 import Prelude
 
-import Data.Argonaut (class DecodeJson, decodeJson, parseJson, printJsonDecodeError)
-import Data.Bifunctor (lmap)
+import Data.Argonaut (class DecodeJson, class EncodeJson, JsonDecodeError, decodeJson, encodeJson, parseJson, stringify)
 import Data.Either (Either)
-import HTTPurple.Json (JsonDecoder(..))
+import HTTPurple.Json (JsonDecoder(..), JsonEncoder(..))
 
-jsonDecoder :: forall json. DecodeJson json => JsonDecoder json
+jsonDecoder :: forall json. DecodeJson json => JsonDecoder JsonDecodeError json
 jsonDecoder = JsonDecoder fromJsonString
   where
-  fromJsonString :: String -> Either String json
-  fromJsonString = (parseJson >=> decodeJson) >>> lmap printJsonDecodeError
+  fromJsonString :: String -> Either JsonDecodeError json
+  fromJsonString = parseJson >=> decodeJson
+
+jsonEncoder :: forall json. EncodeJson json => JsonEncoder json
+jsonEncoder = JsonEncoder toJsonString
+  where
+  toJsonString :: json -> String
+  toJsonString = encodeJson >>> stringify
